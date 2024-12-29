807b70d42e7899dae28492ef29e053c8658d9365
rahul4019 <rahulkashyap4019@gmail.com>
2024-12-29 15:52:01 +0530

    Setup theme for the frontend

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,33 @@
+import { ModeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <ModeToggle />
         <h1 className="text-primary"> Hello World</h1>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+            <Button>click me</Button>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
       </main>
     </div>
   );
---
+++ client/src/components/theme-provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import * as React from "react";
+const NextThemesProvider = dynamic(
+  () => import("next-themes").then((e) => e.ThemeProvider),
+  {
+    ssr: false,
+  },
+);
+
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
+import dynamic from "next/dynamic";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
