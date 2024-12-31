8d2a4fab372fde74cc15915056d9628ef453fe31
rahul4019 <rahulkashyap4019@gmail.com>
2024-12-31 13:31:23 +0530

    Setup backend middlewares and add home route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,26 @@
-import express from "express"
+import express,{Request, Response} from "express"
+import dotenv from 'dotenv'
+import helmet from "helmet"
+import morgan from 'morgan'
+
+dotenv.config()
 
 const app = express()
-const port = 8000
 
-app.listen(port, () => {
-  console.log(`App is up and running at ${port}`)
+app.use(express.json())
+app.use(helmet.crossOriginResourcePolicy({
+  policy: "cross-origin"
+}))
+app.use(morgan("morgan"))
+
+
+app.get("/", (req, res) => {
+ res.status(200).json({message: "Hello from InvDash API"})
+})
+
+
+const port = process.env.PORT  || 8000
+
+app.listen( port, () => {
+  console.log(`Server is running at ${port}`)
 })
