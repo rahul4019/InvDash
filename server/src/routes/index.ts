917767d8bf9df47dff963bc7b5f39803c50d9cf0
rahul4019 <rahulkashyap4019@gmail.com>
2025-01-07 10:40:47 +0530

    Add user routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import dashboardRoutes from "./dashboardRoutes";
 import productRoutes from "./productRoutes";
+import userRoutes from "./userRoutes";
 
 const router = express.Router();
 
 router.use("/", dashboardRoutes);
 router.use("/products", productRoutes);
+router.use("/users", userRoutes);
 
 export default router;
